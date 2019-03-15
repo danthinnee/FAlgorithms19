71b47796d8d0ea90e04afb7958bae2f30697020f
Elisa Danthinne <danthinne.e@husky.neu.edu>
2019-03-15 17:50:08 -0400

    Update ArrayQueue.cc

--- HW4/ArrayQueue.cc
+++ HW4/ArrayQueue.cc
@@ -18,30 +18,30 @@ class ArrayQueue {
 		int count; // number of items current in the queue
 
 	public:
-		ArrayQueue(int c){
+		ArrayQueue(int c){ // constructor
 			QCapacity = c;
 			IDs = new int [c];
 			count = 0;
 			front = 0;
 			back = 0;
 		}
-		~ArrayQueue(){
+		~ArrayQueue(){ // destructor
 			delete[] IDs;
 		}
-		bool isEmpty(){if(count==0) return true; else return false;}
-		bool enqueue(const int newID){
+		bool isEmpty(){if(count==0) return true; else return false;} // if the ArrayQueue is empty return true
+		bool enqueue(const int newID){ // add a new item to the back of the queue
 			if(count < QCapacity){
 				IDs[count]=newID;
 				count++;
 			}
 		}
-		bool dequeue(){
+		bool dequeue(){ // remove item from the front of the queue
 			count--;
 			for(int i=0; i<count; i++){
 				IDs[i] = IDs[i+1];
 			}
 		}
-		int peekFront(){
+		int peekFront(){ // return id of front item
 			return IDs[0];
 		}
 		int queueLength(){ return count; } // returns the current number of items in teh queue
